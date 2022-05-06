c428309a0db395cce083b12840a53f501e4a381a
Sean Orosz <seanorosz@yahoo.com>
2022-05-06 15:22:34 -0400

    started work on new layout

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -8,11 +8,6 @@ class Todo extends React.Component {
             <div onClick={() => changeTodo(this.props.id)} className={this.props.total === 1 ? 'only-todo todo-wrapper' : this.props.id === 1 ? 'first-todo todo-wrapper' : this.props.id === this.props.total ? 'last-todo todo-wrapper' : 'middle-todo todo-wrapper'}>
                 <div className='todo-div'>
                     <h2 className='todo-title'>{this.props.title}</h2>
-                    <div className='todo-description-div'>
-                        <span className='todo-description'>{this.props.description}</span>
-                    </div>
-                    <h3>{this.props.status}</h3>
-                    <span>{this.props.deadlineString}</span>
                 </div>
             </div>
         );
