3374d4324afad1246de3fec2860d9fa0598976fa
Sean Orosz <seanorosz@yahoo.com>
2022-06-11 15:06:14 -0400

    fixed date-time input display for ios

--- src/Components/FullTodo.js
+++ src/Components/FullTodo.js
@@ -7,10 +7,10 @@ class FullTodo extends React.Component {
             <div className={classString}>
                 <input className='create-todo-element' placeholder='Title' type='text' id='title' />
                 {this.props.fullTodo.description === undefined ? 
-                    <input className='todo-button' type='button' value='Add Description' onClick={() => this.props.addDescription()} /> :
+                    <input className='todo-button' type='button' value='+ Description' onClick={() => this.props.addDescription()} /> :
                     <div className='create-todo-element-holder'>
                         <div className='remove-circle' id='description-remove-circle' onClick={() => this.props.removeElement('Description')}>
-                            <span  className='remove-x'>x</span>
+                            <span  className='remove-x'>X</span>
                         </div>
                         <textarea className='create-todo-element' placeholder='Enter Description Here...' id='description' />
                     </div>
@@ -18,11 +18,11 @@ class FullTodo extends React.Component {
                 {this.props.fullTodo.deadline ? 
                     <div className='create-todo-element-holder'>
                         <div className='remove-circle' id='deadline-remove-circle' onClick={() => this.props.removeElement('Deadline')}>
-                            <span  className='remove-x' >x</span>
+                            <span  className='remove-x' >X</span>
                         </div>
                         <input className='create-todo-element' type='datetime-local' id='date-time' onChange={() => this.props.updateDisplayTodo()} /> 
                     </div> :
-                    <input className='todo-button' type='button' id='add-deadline' value='Add Deadline' onClick={() => this.props.addDeadline()} />
+                    <input className='todo-button' type='button' id='add-deadline' value='+ Deadline' onClick={() => this.props.addDeadline()} />
                 }
                 <div className='create-todo-element' id='status-div'>
                     <input id='not-started' className='status-element' type='radio' name='progress' value='Not Started' onChange={() => this.props.updateDisplayTodo()}/><label htmlFor='not-started'>Not Started</label>
